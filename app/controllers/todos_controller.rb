2827546c31db25ba47180dff5ae8d4bd07403136
Shiraz <shirazbond1@gmail.com>
2021-02-17 23:02:22 +0400

    Displayed a single todo in /todos/id

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,10 +1,17 @@
 # todos_controller.rb
 class TodosController < ApplicationController
   def index
-    render plain: "Hello world, this is Shizzy"
+    render plain: "Hello world, this is Shiraz"
   end
 
   def db
-    render plain: Todo.all.to_a
+    render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.
+             join("\n")
+  end
+
+  def show
+    id = params[:id]
+    todo = Todo.find(id)
+    render plain: todo.to_pleasant_string
   end
 end
