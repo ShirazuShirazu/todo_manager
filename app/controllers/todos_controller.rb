219c520f1db0f03b8f556a09b5682ed57d207942
Shiraz <shirazbond1@gmail.com>
2021-02-19 00:22:23 +0400

    Finished the todo handler with basic CRUD operations.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,17 +1,51 @@
 # todos_controller.rb
 class TodosController < ApplicationController
-  def index
-    render plain: "Hello world, this is Shiraz"
-  end
+  skip_before_action :verify_authenticity_token
 
-  def db
-    render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.
+  def index
+    render plain: Todo.all.order(:due_date).to_a.map { |todo| todo.to_pleasant_string }.
              join("\n")
   end
 
   def show
     id = params[:id]
-    todo = Todo.find(id)
-    render plain: todo.to_pleasant_string
+    # render plain: Todo.exists?(id)
+    todo = Todo.find_by(id: id)
+
+    if todo == nil
+      render plain: "No todo found with id: #{id}"
+    else
+      render plain: todo.to_pleasant_string
+    end
+  end
+
+  def create
+    Todo.create!(todo_text: params[:todo_text], due_date: params[:due_date], completed: false, id: params[:id])
+    render plain: "Todo created!"
+  end
+
+  def update
+    id = params[:id]
+    todo = Todo.find_by(id: id)
+
+    if todo == nil
+      render plain: "No todo found with id: #{id}"
+    else
+      todo.completed = true
+      todo.save
+      render plain: "Updated todo."
+    end
+  end
+
+  def destroy
+    id = params[:id]
+    todo = Todo.find_by(id: id)
+
+    if todo == nil
+      render plain: "No todo found with id: #{id}"
+    else
+      Todo.destroy(id)
+      render plain: "Todo with id: #{id}, was deleted."
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get "todos", to: "todos#db"
+  get "todos", to: "todos#index"
   get "todos/:id", to: "todos#show"
+  post "todos", to: "todos#create"
+  patch "todos/:id", to: "todos#update"
+  delete "todos/:id", to: "todos#destroy"
 end
